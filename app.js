133dde1ce7316f16c25028b15c47735d6fbf09ca
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-09-02 15:59:15 +0530

    Function to fetch stores from stores.js

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ const attribution =
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by Saifi❤️';
 const tileLayer = L.tileLayer(tileUrl, { attribution });
 tileLayer.addTo(myMap);
+
+function generateList() {
+  const ul = document.querySelector('.list');
+  storeList.forEach((shop) => {
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const a = document.createElement('a');
+    const p = document.createElement('p');
+    a.addEventListener('click', () => {
+        flyToStore(shop);
+    });
+    div.classList.add('shop-item');
+    a.innerText = shop.properties.name;
+    a.href = '#';
+    p.innerText = shop.properties.address;
+
+    div.appendChild(a);
+    div.appendChild(p);
+    li.appendChild(div);
+    ul.appendChild(li);
+  });
+}
+
+generateList();
